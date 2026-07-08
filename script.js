93777c5dfb3d5645728534778b30099eb4e262b8
flamax92 <joes-adventskalender@web.de>
2026-07-08 22:46:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Website geladen");
+document.querySelectorAll(".gallery img").forEach(img => {
+
+img.addEventListener("mousedown", () => {
+
+img.classList.add("zoom");
+
+});
+
+img.addEventListener("mouseup", () => {
+
+img.classList.remove("zoom");
+
+});
+
+img.addEventListener("mouseleave", () => {
+
+img.classList.remove("zoom");
+
+});
+
+img.addEventListener("touchstart", () => {
+
+img.classList.add("zoom");
+
+});
+
+img.addEventListener("touchend", () => {
+
+img.classList.remove("zoom");
+
+});
+
+});
