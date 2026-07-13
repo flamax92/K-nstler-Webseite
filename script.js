6b8219918a688102cf5df26f847b74d18bd63512
flamax92 <joes-adventskalender@web.de>
2026-07-13 12:46:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 console.log("Website geladen");
 
+<script>
+
+let slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function changeSlide(){
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add("active");
+
+}
+
+
+setInterval(changeSlide, 2000);
+
+</script>
