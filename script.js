9fd9ea67ac7c8b61ec94a2f4c18502535eafd65d
flamax92 <joes-adventskalender@web.de>
2026-07-13 12:56:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,117 @@
-console.log("Website geladen");
+const projects={
 
-<script>
 
-let slides = document.querySelectorAll(".slide");
+"dance-for-increasing-distances":{
 
-let currentSlide = 0;
+title:"Dance for Increasing Distances",
 
-function changeSlide(){
 
-    slides[currentSlide].classList.remove("active");
+hero:[
 
-    currentSlide++;
+"images/dance-for-increasing-distances/hero1.jpg",
 
-    if(currentSlide >= slides.length){
-        currentSlide = 0;
-    }
+"images/dance-for-increasing-distances/hero2.jpg",
 
-    slides[currentSlide].classList.add("active");
+"images/dance-for-increasing-distances/hero3.jpg"
+
+],
+
+
+gallery:[
+
+"images/dance-for-increasing-distances/gallery1.jpg",
+
+"images/dance-for-increasing-distances/gallery2.jpg"
+
+]
+
+},
+
+
+
+"port-de-bras":{
+
+title:"Port de Bras",
+
+hero:[
+
+"images/port-de-bras/hero1.jpg",
+
+"images/port-de-bras/hero2.jpg"
+
+],
+
+
+gallery:[
+
+"images/port-de-bras/gallery1.jpg"
+
+]
+
+}
+
+};const slider=document.getElementById("slider");
+
+
+project.hero.forEach((src,index)=>{
+
+
+let div=document.createElement("div");
+
+div.className="slide";
+
+
+if(index===0){
+
+div.classList.add("active");
 
 }
 
 
-setInterval(changeSlide, 2000);
+div.innerHTML=
+`
+<img src="${src}">
+`;
 
-</script>
+
+slider.appendChild(div);
+
+
+});
+
+
+let slides=document.querySelectorAll(".slide");
+
+
+let current=0;
+
+
+setInterval(()=>{
+
+
+slides[current].classList.remove("active");
+
+
+current=(current+1)%slides.length;
+
+
+slides[current].classList.add("active");
+
+
+},5000);const gallery=document.getElementById("gallery");
+
+
+project.gallery.forEach(src=>{
+
+
+let img=document.createElement("img");
+
+img.src=src;
+
+img.alt=project.title;
+
+
+gallery.appendChild(img);
+
+
+});
